d535fd15aea5035c19731f85e0c1bb216ca31577
daphnalex <daphne.bordel@gmail.com>
2019-02-14 14:47:30 +0100

    drop folder back

---
+++ back/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: "mongodb://daphnalex:1234azerty@ds145474.mlab.com:45474/mma-plateforme",
+    secret: 'Titi et Toto sont sur un bâteau',
+    api: 'http://localhost:5001/api/'
+}
